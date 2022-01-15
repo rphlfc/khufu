3d7dc22fdab55bd88ad2bdf3efbd852f019f13a0
Raphael Cerqueira <raphaelfc@gmail.com>
2022-01-15 15:26:45 -0300

    Updates

---
+++ precache-manifest.966b4dd998d9e2ffe785b5a44bf09db7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156eadef025d0baf457a42d624cfaad7",
+    "url": "/khufu/index.html"
+  },
+  {
+    "revision": "4aa3d4e8e66854bb45b4",
+    "url": "/khufu/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "117f6efe4af0dace4005",
+    "url": "/khufu/static/css/main.c63a64cc.chunk.css"
+  },
+  {
+    "revision": "4aa3d4e8e66854bb45b4",
+    "url": "/khufu/static/js/2.56188b8c.chunk.js"
+  },
+  {
+    "revision": "39e3338d6fa0463e5f9ad984b8c6a671",
+    "url": "/khufu/static/js/2.56188b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b46e8272295a519f44",
+    "url": "/khufu/static/js/3.d15e4193.chunk.js"
+  },
+  {
+    "revision": "117f6efe4af0dace4005",
+    "url": "/khufu/static/js/main.2622b6d8.chunk.js"
+  },
+  {
+    "revision": "3ff61e0abe43711e6797",
+    "url": "/khufu/static/js/runtime-main.ec1c2682.js"
+  }
+]);
